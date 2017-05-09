08a82778abd72645f70ee3f14ec6c83a805755ed
men15444380 <v.mendes1@arts.ac.uk>
2017-05-09 11:10:19 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,5 +1,17 @@
+
+var cnv;
+var g;
+
+var t = 0;
+
 function setup() {
- createCanvas(400, 600);}
+
+  cnv= createCanvas(400, 600);
+  cnv.touchMoved(changeGray);
+  // background(0, 0, 255);
+  g = 400 ;
+ 
+}
 
 function draw() {
   background(color(0, 0, 255));
@@ -9,20 +21,41 @@ fill(c);
 noStroke(); 
   triangle(17, 0, 400, 0, 400, 375);
   
-var c = color('magenta');  
+var c = color(255,20,147);  
 fill(c);  
 noStroke(); 
 triangle(0, 0, 0, 245, 250, 0);
   
-var c = color('magenta');  
-fill(c);  
+var c = color(255,20,147)
+;fill(c);  
 noStroke(); 
 triangle(0, 600, 600, 600, 600, 0);
 
-var c = color('hsl(160, 100%, 50%)');  
+// var c = color(64,224,208);  
+
+if(t % 2 == 0) {
+  c = color(255,0,0);
+} else {
+  c = color(255,0,255);
+}
+
 fill(c);
 noStroke();  
 rect(135, 113, 176, 176);
-  
-  
+
+// rect(g);
 }
+
+// this function fires only when cnv is clicked
+function changeGray() {
+  if(g=400) 
+  mouseClicked=this.changeGrey
+  random(0, 255);
+
+}
+
+function mousePressed() {
+  t++;
+  console.log(t);
+}
+  
